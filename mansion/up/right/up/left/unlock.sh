c1e54745e684c950678d3effbcdaa9d48c8e172f
niemisami <sami@nieminen.fi>
2020-03-17 20:48:28 +0200

    Add fifth task

--- mansion/up/right/up/left/unlock.sh
+++ mansion/up/right/up/left/unlock.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-ANSWER="$(echo $1 | cksum)"
-CORRECT_ANSWER=
+S1="$(ls peg* | wc -l)"
 
-if [[ $ANSWER = $CORRECT_ANSWER ]]; then
+if [ $S1 = 0 ]; then
+  echo "You have lost all your pegs. The original peg mysteriously manifested back to the hole"
+  echo "peg made of clay" > peg
+  exit 0
+fi
+
+if [[ $S1 > 5 ]]; then
+  echo "You have created too many pegs. Use powerful spell \"rm peg*\" to get rid of them all and then run \"./unlock.sh\" again to start over"
+  exit 0
+fi
+
+if [ $S1 = 5 ]; then
   echo "Correct answer! 
+The ground starts to shake as the door slowly slides into the wall.
 You unlocked the next door."
 else
   echo "Answer is not correct. Please try again."
