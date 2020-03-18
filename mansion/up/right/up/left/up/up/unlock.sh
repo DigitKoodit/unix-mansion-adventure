8e17b42f88a31e4a06ee562499da205d46c3c104
niemisami <sami@nieminen.fi>
2020-03-18 17:12:49 +0200

    Add unlock to last task

--- mansion/up/right/up/left/up/up/unlock.sh
+++ mansion/up/right/up/left/up/up/unlock.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-ANSWER="$(echo $1 | cksum)"
-CORRECT_ANSWER=
+if [[ ! -d cell-left-front ]]; then
+  echo "Oh no cells got destroyed!
+If this happens run \"git checkout .\" to start over."
+  exit 1
+fi
 
-if [[ $ANSWER = $CORRECT_ANSWER ]]; then
+S1="$(ls villager* | wc -l)"
+S2="$(ls cell*/villain* | wc -l)"
+
+if [[ $S2 > 0 ]]; then
+  echo "Quicky beat villains before they attack!"
+  exit 0
+fi
+
+if [[ $S1 < 10 ]]; then
+  echo "Not all villagers are safe yet! Please hurry!"
+  exit 0
+fi
+
+if [[ $S1 = 10 ]]; then
   echo "YOU DID IT! You freed the villagers!
 
 Congratulations! 
@@ -15,6 +31,6 @@ Thank you for your bravery noble hero!
 The end.
 "
 else
-  echo "Answer is not correct. Please try again."
+  echo " Please try again."
 fi
 
